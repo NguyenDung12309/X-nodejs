9bdf3d07c8ccee216c1692615118d928568dd06a
htv-dungnn <dungnn@hybrid-technologies.vn>
2024-07-26 15:21:17 +0700

    feat: handle route with middleware and controller

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-const message = 'hello world'
+import express from 'express'
+import userRouter from './routes/user.js'
+const app = express()
+const port = 3000
 
-console.log(message)
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use('/api', userRouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
