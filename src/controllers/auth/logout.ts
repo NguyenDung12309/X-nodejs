9ae9c0d7ffe68f9f0e8f8244e1782b6415aa6609
htv-dungnn <dungnn@hybrid-technologies.vn>
2024-08-14 09:42:48 +0700

    fix: update import

--- src/controllers/auth/logout.ts
+++ src/controllers/auth/logout.ts
@@ -1,12 +1,12 @@
 import { handleResponseSuccess } from '@/helpers/handler'
-import { reqLogout } from '@/models/dto/auth/logout'
+import { reqRefreshToken } from '@/models/dto/token/token'
 import { databaseService } from '@/services/db'
 import { tokenService } from '@/services/token'
 import { userService } from '@/services/user'
 import { Controller } from '@/types/type'
 import { DeleteResult } from 'mongodb'
 
-export const logoutController: Controller<reqLogout> = async (req, res) => {
+export const logoutController: Controller<reqRefreshToken> = async (req, res) => {
   const token = req.body.refresh_token
 
   const result = await databaseService.refreshToken.deleteOne({ token })
