199a9697c8a711aaee2c3277be43205745252967
dungnn <trandung070899@gmail.com>
2024-09-29 11:20:39 +0700

    feat: create and delete bookmark

--- src/routes/bookmark.ts
+++ src/routes/bookmark.ts
@@ -1,4 +1,5 @@
 import { API_CONST } from '@/constraints/api'
+import { HTTP_STATUS } from '@/constraints/httpStatus'
 import { wrapRequestHandler } from '@/helpers/handler'
 import { validatorMiddleWare } from '@/helpers/validate'
 import express from 'express'
@@ -7,13 +8,17 @@ const router = express.Router()
 
 router.post(
   API_CONST.bookmark,
-  validatorMiddleWare({ validator: 'createBookmarkValidate' }),
+  validatorMiddleWare({ validator: 'createBookmarkValidate', initStatusCode: HTTP_STATUS.BAD_REQUEST }),
   wrapRequestHandler('createBookmarkController')
 )
 
 router.delete(
   API_CONST.bookmark,
-  validatorMiddleWare({ validator: 'deleteBookmarkValidate', location: 'query' }),
+  validatorMiddleWare({
+    validator: 'deleteBookmarkValidate',
+    location: 'query',
+    initStatusCode: HTTP_STATUS.BAD_REQUEST
+  }),
   wrapRequestHandler('deleteBookmarkController')
 )
 
